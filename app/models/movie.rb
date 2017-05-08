ae41694d7b16d6b90ad81f7fd43b6e8f9d5780ae
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-05-07 20:38:48 -0500

    update

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,4 +11,7 @@ validates:duration,numericality:{only_integer:true,:greater_than_or_equal_to=>0,
 
 # - description: no rules
 # - image_url: no rules
+
+  belongs_to(:director,:class_name=>"Director",:foreign_key=>"director_id")
+  has_many(:characters,:class_name=>"Character",:foreign_key=>"movie_id")
 end
