59768c1df342dbf2ae04b1647aa491d2990ef066
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-05-09 08:47:49 -0500

    updated

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,4 +14,5 @@ validates:duration,numericality:{only_integer:true,:greater_than_or_equal_to=>0,
 
   belongs_to(:director,:class_name=>"Director",:foreign_key=>"director_id")
   has_many(:characters,:class_name=>"Character",:foreign_key=>"movie_id")
+  has_many :actors, :through => :characters
 end
