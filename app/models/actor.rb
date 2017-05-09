b12c28016eb1d26d8f8f1dc4efef21e995a9baa7
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-05-09 08:50:18 -0500

    update

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,5 @@ class Actor < ApplicationRecord
 # - bio: no rules
 # - image_url: no rules
 has_many(:characters,:class_name=>"Character",:foreign_key=>"actor_id")
+has_many :movies, :through => :characters
 end
